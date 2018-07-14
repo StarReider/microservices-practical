41b9dff65bfc5a09c17a6b840322ffd876a3c849
Roman <Roman@192.168.99.1>
2018-07-15 02:02:50 +0300

    add multiplication service implementation

--- social-multiplication/src/test/java/microservices/book/multiplication/service/MultiplicationServiceTest.java
+++ social-multiplication/src/test/java/microservices/book/multiplication/service/MultiplicationServiceTest.java
@@ -26,11 +26,9 @@ public class MultiplicationServiceTest {
 	public void createRandomMultiplicationTest() {
 		// given (our mocked Random Generator service will return first 50, then 30)
 		
-		given(randomGeneratorService.generateRandomFactor()).
-		willReturn(50, 30);
+		given(randomGeneratorService.generateRandomFactor()).willReturn(50, 30);
 		// when
-		Multiplication multiplication = multiplicationService.
-		createRandomMultiplication();
+		Multiplication multiplication = multiplicationService.createRandomMultiplication();
 		// then
 		assertThat(multiplication.getFactorA()).isEqualTo(50);
 		assertThat(multiplication.getFactorB()).isEqualTo(30);
