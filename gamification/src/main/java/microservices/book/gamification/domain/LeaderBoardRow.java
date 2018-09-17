f3d04ba5b59b4e2bb864642570eeb5adb069afec
bezugomonnov <bezugomonnov@WSM-0768.netcracker.com>
2018-09-17 19:29:47 +0300

    LeaderBoardRow fix

--- gamification/src/main/java/microservices/book/gamification/domain/LeaderBoardRow.java
+++ gamification/src/main/java/microservices/book/gamification/domain/LeaderBoardRow.java
@@ -1,5 +1,10 @@
 package microservices.book.gamification.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
 /**
 * Represents a line in our Leaderboard: it links a user to a
 total score.
