dca0e98cecb2d2de00d8cbe9f9a2d4556ddeb8fb
bezugomonnov <bezugomonnov@WSM-0768.netcracker.com>
2018-09-18 20:06:14 +0300

    gameserviceimpl test

--- gamification/src/test/java/microservices/book/gamification/service/GameServiceImplTest.java
+++ gamification/src/test/java/microservices/book/gamification/service/GameServiceImplTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import microservices.book.gamification.domain.Badge;
+import microservices.book.gamification.domain.BadgeCard;
 import microservices.book.gamification.domain.GameStats;
 import microservices.book.gamification.domain.ScoreCard;
 import microservices.book.gamification.repository.BadgeCardRepository;
@@ -19,6 +20,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class GameServiceImplTest {
 
@@ -39,14 +43,14 @@ public class GameServiceImplTest {
 	
 	@Test
 	public void processFirstCorrectAttemptTest() {
-		
 		// given
         Long userId = 1L;
         Long attemptId = 8L;
         int totalScore = 10;
         ScoreCard scoreCard = new ScoreCard(userId, attemptId);
         
-        given(scoreCardRepository.getTotalScoreForUser(userId)).willReturn(totalScore);
+        given(scoreCardRepository.getTotalScoreForUser(userId))
+        	.willReturn(totalScore);
         
         given(scoreCardRepository.findByUserIdOrderByScoreTimestampDesc(userId))
         	.willReturn(Collections.singletonList(scoreCard));
@@ -61,4 +65,58 @@ public class GameServiceImplTest {
         assertThat(iteration.getScore()).isEqualTo(scoreCard.getScore());
         assertThat(iteration.getBadges()).containsOnly(Badge.FIRST_WON);
 	}
+	
+	@Test
+    public void processCorrectAttemptForScoreBadgeTest() {
+		// given
+        Long userId = 1L;
+        Long attemptId = 29L;
+        int totalScore = 100;
+        BadgeCard firstWonBadge = new BadgeCard(userId, Badge.FIRST_WON);
+        
+        given(scoreCardRepository.getTotalScoreForUser(userId))
+        	.willReturn(totalScore);
+        
+        // this repository will return the just-won score card
+        given(scoreCardRepository.findByUserIdOrderByScoreTimestampDesc(userId))
+        	.willReturn(createNScoreCards(10, userId));
+        
+        // the first won badge is already there
+        given(badgeCardRepository.findByUserIdOrderByBadgeTimestampDesc(userId))
+        	.willReturn(Collections.singletonList(firstWonBadge));
+        
+        // when
+        GameStats iteration = gameService.newAttemptForUser(userId, attemptId, true);
+        
+        // assert - should score one card and win the badge BRONZE
+        assertThat(iteration.getScore()).isEqualTo(ScoreCard.DEFAULT_SCORE);
+        assertThat(iteration.getBadges()).containsOnly(Badge.BRONZE_MULTIPLICATOR);
+	}
+	
+	@Test
+    public void retrieveStatsForUserTest() {
+		// given
+        Long userId = 1L;
+        int totalScore = 1000;
+        BadgeCard badgeCard = new BadgeCard(userId, Badge.SILVER_MULTIPLICATOR);
+        
+        given(scoreCardRepository.getTotalScoreForUser(userId))
+        	.willReturn(totalScore);
+        
+        given(badgeCardRepository.findByUserIdOrderByBadgeTimestampDesc(userId))
+        	.willReturn(Collections.singletonList(badgeCard));
+        
+        // when
+        GameStats stats = gameService.retrieveStatsForUser(userId);
+        
+        // assert - should score one card and win the badge FIRST_WON
+        assertThat(stats.getScore()).isEqualTo(totalScore);
+        assertThat(stats.getBadges()).containsOnly(Badge.SILVER_MULTIPLICATOR);
+	}
+	
+	private List<ScoreCard> createNScoreCards(int n, Long userId) {
+        return IntStream.range(0, n)
+                .mapToObj(i -> new ScoreCard(userId, (long)i))
+                .collect(Collectors.toList());
+    }
 }
