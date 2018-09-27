0a8f2839e0df7b0780363f38c37a9632d41a2750
bezugomonnov <bezugomonnov@WSM-0768.netcracker.com>
2018-09-27 17:22:44 +0300

    get result by id service

--- social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationResultAttemptService.java
+++
@@ -1,8 +0,0 @@
-package microservices.book.multiplication.service;
-
-import microservices.book.multiplication.domain.MultiplicationResultAttempt;
-
-public interface MultiplicationResultAttemptService {
-	
-	MultiplicationResultAttempt getMultiplicationResultAttempt(final Long attemptId);
-}
--- social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationService.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationService.java
@@ -20,5 +20,19 @@ public interface MultiplicationService {
 	*/
 	boolean checkAttempt(final MultiplicationResultAttempt resultAttempt);
 	
-	List<MultiplicationResultAttempt> getStatsForUser(String userAlias);
+	/**
+     * Gets the statistics for a given user.
+     *
+     * @param userAlias the user's alias
+     * @return a list of {@link MultiplicationResultAttempt} objects, being the past attempts of the user.
+     */
+	List<MultiplicationResultAttempt> getStatsForUser(final String userAlias);
+	
+	/**
+     * Gets an attempt by its id
+     *
+     * @param resultId the identifier of the attempt
+     * @return the {@link MultiplicationResultAttempt} object matching the id, otherwise null.
+     */
+	MultiplicationResultAttempt getResultById(final Long attemptId);
 }
