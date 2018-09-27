cd2fcbd1a898459aae0e64bbddca05f925bb8ff9
bezugomonnov <bezugomonnov@WSM-0768.netcracker.com>
2018-09-27 19:36:57 +0300

    fix jackson problem with MultiplicationSolvedEvent

--- gamification/src/main/java/microservices/book/gamification/event/MultiplicationSolvedEvent.java
+++ gamification/src/main/java/microservices/book/gamification/event/MultiplicationSolvedEvent.java
@@ -2,16 +2,17 @@ package microservices.book.gamification.event;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 /**
 * Event that models the fact that a {@link microservices.book.multiplication.domain.Multiplication}
 * has been solved in the system. Provides some context information about the multiplication.
 */
-@RequiredArgsConstructor
 @Getter
 @ToString
 @EqualsAndHashCode
@@ -22,4 +23,13 @@ public class MultiplicationSolvedEvent implements Serializable {
 	private final Long multiplicationResultAttemptId;
 	private final Long userId;
 	private final boolean correct;
+	
+	@JsonCreator
+    public MultiplicationSolvedEvent(@JsonProperty("multiplicationResultAttemptId") Long multiplicationResultAttemptId,
+    		@JsonProperty("userId") Long userId,
+    		@JsonProperty("correct") boolean correct) {
+		this.multiplicationResultAttemptId = multiplicationResultAttemptId;
+		this.userId = userId;
+        this.correct = correct;
+    } 
 }
