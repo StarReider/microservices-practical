f6e66168ff3bbb0dbb4bdd19f987e3d7adbefb51
Bezugomonnov <Bezugomonnov@WSM-0768.netcracker.com>
2018-11-15 16:26:39 +0300

    additional logging

--- social-multiplication/src/main/java/microservices/book/multiplication/controller/MultiplicationResultAttemptController.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/controller/MultiplicationResultAttemptController.java
@@ -3,6 +3,7 @@ package microservices.book.multiplication.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,18 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
 import microservices.book.multiplication.domain.MultiplicationResultAttempt;
 import microservices.book.multiplication.service.MultiplicationService;
 
+@Slf4j
 @RestController
 @RequestMapping("/results")
 public class MultiplicationResultAttemptController {
 
 	private final MultiplicationService multiplicationService;
+	private final int serverPort;
 	
 	@Autowired
-	MultiplicationResultAttemptController(final MultiplicationService multiplicationService) {
+	MultiplicationResultAttemptController(final MultiplicationService multiplicationService, @Value("${server.port}") int serverPort) {
 		this.multiplicationService = multiplicationService;
+		this.serverPort = serverPort;
 	}
 	
 	@PostMapping
@@ -46,7 +51,8 @@ public class MultiplicationResultAttemptController {
 	}
 	
 	@GetMapping("/{resultId}")
-	ResponseEntity<MultiplicationResultAttempt> getResultById(@PathVariable("resultId") Long resultId) {		
+	ResponseEntity<MultiplicationResultAttempt> getResultById(@PathVariable("resultId") Long resultId) {	
+		log.info("Retrieving result {} from server @ {}", resultId, serverPort);
 		return ResponseEntity.ok(multiplicationService.getResultById(resultId));		
 	}
 }
