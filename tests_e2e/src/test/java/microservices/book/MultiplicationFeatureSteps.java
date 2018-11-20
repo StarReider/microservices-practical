96bc2ac8531cdd776b7bdd57cec53824357f8c79
bezugomonnov <bezugomonnov@WSM-0768.netcracker.com>
2018-11-20 20:09:49 +0300

    get rid of from unnecessary feature steps

--- tests_e2e/src/test/java/microservices/book/MultiplicationFeatureSteps.java
+++ tests_e2e/src/test/java/microservices/book/MultiplicationFeatureSteps.java
@@ -3,7 +3,6 @@ package microservices.book;
 import java.util.List;
 import java.util.stream.IntStream;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -53,30 +52,22 @@ public class MultiplicationFeatureSteps {
 	@Then("^the user gets (\\d+) points for the attempt$")
 	public void the_user_gets_points_for_the_attempt(final int points) throws Throwable {
 		long attemptId = lastAttemptResponse.getId();
+		
 		Thread.sleep(2000);
+		
 		int score = app.getScoreForAttempt(attemptId).getScore();
 		assertThat(score).isEqualTo(points);
 	}
 
 	@Then("^the user gets the ([^\\s]+) badge$")
 	public void the_user_gets_the_type_badge(final String badgeType) throws Throwable {
+		
 		long userId = lastAttemptResponse.getUser().getId();
 		Thread.sleep(200);
+		
 		lastStatsResponse = app.getStatsForUser(userId);
 		List<String> userBadges = lastStatsResponse.getBadges();
 		assertThat(userBadges).contains(badgeType);
-	}
-
-	@Given("^the user john_snow sends (\\d+) right attempts$")
-	public void the_user_john_snow_sends_right_attempts(int arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
-
-	@Given("^the user gets the FIRST_WON badge$")
-	public void the_user_gets_the_FIRST_WON_badge() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
 	}
 
 	@Then("^the user does not get any badge$")
@@ -91,36 +82,6 @@ public class MultiplicationFeatureSteps {
 		} else {
 			assertThat(userBadges).isEqualTo(lastStatsResponse.getBadges());
 		}
-	}
-
-	@When("^the user john_snow sends (\\d+) wrong attempts$")
-	public void the_user_john_snow_sends_wrong_attempts(int arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
-
-	@Then("^the user gets a response indicating the attempt is wrong$")
-	public void the_user_gets_a_response_indicating_the_attempt_is_wrong() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
-
-	@Then("^the user gets the BRONZE_MULTIPLICATOR badge$")
-	public void the_user_gets_the_BRONZE_MULTIPLICATOR_badge() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
-
-	@Then("^the user gets the SILVER_MULTIPLICATOR badge$")
-	public void the_user_gets_the_SILVER_MULTIPLICATOR_badge() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
-
-	@Then("^the user gets the GOLD_MULTIPLICATOR badge$")
-	public void the_user_gets_the_GOLD_MULTIPLICATOR_badge() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
 	}
 	
 	@Given("^the user has (\\d+) points$")
