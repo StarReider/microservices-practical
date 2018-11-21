6f581323552d467496d97b434fe80bd6e21e8b65
bezugomonnov <bezugomonnov@WSM-0768.netcracker.com>
2018-11-21 16:47:00 +0300

    cleanup fix

--- social-multiplication/src/main/java/microservices/book/multiplication/service/AdminServiceImpl.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/service/AdminServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import microservices.book.multiplication.repository.MultiplicationRepository;
+import microservices.book.multiplication.repository.MultiplicationResultAttemptRepository;
 import microservices.book.multiplication.repository.UserRepository;
 
 @Profile("test")
@@ -12,14 +13,18 @@ public class AdminServiceImpl implements AdminService {
 
 	private MultiplicationRepository multiplicationRepository;
     private UserRepository userRepository;
+    private MultiplicationResultAttemptRepository multiplicationResultAttemptRepository;
     
-    public AdminServiceImpl(final MultiplicationRepository multiplicationRepository, final UserRepository userRepository) {
+    public AdminServiceImpl(final MultiplicationRepository multiplicationRepository, 
+    						final UserRepository userRepository,
+    						final MultiplicationResultAttemptRepository multiplicationResultAttemptRepository) {
     	this.multiplicationRepository = multiplicationRepository;
     	this.userRepository = userRepository;
     }
     
 	@Override
 	public void deleteDatabaseContents() {
+		multiplicationResultAttemptRepository.deleteAll();
 		multiplicationRepository.deleteAll();
 		userRepository.deleteAll();
 	}
